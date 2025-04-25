073de0a7119a562703c999dfd19fd82aa1aa4f87
Uzair Mohammed <uzair.hamed@gmail.com>
2025-04-25 02:31:51 -0700

    Revert "Fixed issue with 01_02_array_max.rs"
    This reverts commit b9bf5e46f7a9d178abc6ce8c5e1d45dd40cbc907.

--- lessons/01/exercises/tests/02_array_max.rs
+++ lessons/01/exercises/tests/02_array_max.rs
@@ -6,17 +6,8 @@
 // The input argument is an array of ten `i32` integers.
 // How does that type look like in Rust?
 
-fn find_largest(mut nums: [i32; 10]) -> i32 {
-    for i in 0..nums.len() {
-        for j in 0..nums.len() - 1 {
-            if nums[j] > nums[j + 1] {
-                let temp = nums[j];
-                nums[j] = nums[j + 1];
-                nums[j + 1] = temp;
-            }
-        }
-    }
-    nums[0]
+fn find_largest(nums: [i32; 10]) -> i32 {
+    nums.sort()[0]
 }
 
 /// Below you can find a set of unit tests.
